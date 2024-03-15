2c63e38f9b4994cd6adefcf5259fa3c5122c3584
sergey <sapokrovsky@gmail.com>
2024-03-15 23:18:48 +0300

    init project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
+import {getCurrentID,taskGet} from "./api/bitrix-api"
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    getCurrentID().then(console.log)
+    taskGet().then(console.log)
+  }, []);
 
-  return
-  
-    <></>
+  return(
+  <>
+    <p>Hello World</p>
+  </>)
 }
 
 export default App;
--- src/api/bitrix-api.js
+++ src/api/bitrix-api.js
@@ -1,28 +1,28 @@
 import {BX24} from "../utils/utils";
 
-// export function getCurrentID() {
-//   return new Promise((resolve, reject) => {
-//     try {
-//       BX24.init(function () {
-//         resolve(BX24.placement.info());
-//       });
-//     } catch (err) {
-//       reject(err);
-//     }
-//   });
-// }
+export function getCurrentID() {
+  return new Promise((resolve, reject) => {
+    try {
+      BX24.init(function () {
+        resolve(BX24.placement.info());
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
 
-// export function taskGet(id) {
-//   return new Promise((resolve, reject) => {
-//     BX24.callMethod("tasks.task.get", { taskId: id }, (result) => {
-//       if (result.error()) {
-//         reject(result.error());
-//       } else {
-//         resolve(result.data());
-//       }
-//     });
-//   });
-// }
+export function taskGet(id) {
+  return new Promise((resolve, reject) => {
+    BX24.callMethod("crm.item.list",{entityTypeId: 180},(result) => {
+      if (result.error()) {
+        reject(result.error());
+      } else {
+        resolve(result.data());
+      }
+    });
+  });
+}
 
 // export function taskComplete(id) {
 //   return new Promise((resolve, reject) => {
@@ -47,3 +47,4 @@ import {BX24} from "../utils/utils";
 //     });
 //   });
 // }
+
