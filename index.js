c5ee258ada1b81f2427d5b5d8a175bcf2bf8b701
sergey <sapokrovsky@gmail.com>
2024-04-20 01:52:12 +0300

    init

--- index.js
+++ index.js
@@ -1,73 +1,54 @@
-const GOLOVNOY = "UF_CRM_1713200303";
+const GOLOVNOY = "UF_CRM_1713469133";
 const DOCHERNIY = "UF_CRM_1713278591";
 
-fetch("https://itr24.bitrix24.ru/rest/1/9fw61yqqc6se2xeq/crm.company.list.json")
-  .then((res) => res.json())
-  .then((result) => {
-    result.result.map((item) => {
-      // console.log(item);
-      fetch(
-        "https://itr24.bitrix24.ru/rest/1/54eoiyquodgp4a4g/crm.company.get.json",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ id: item.ID }),
-        }
-      )
-        .then((res) => res.json())
-        .then((result) => {
-          if (result.result[GOLOVNOY]) {
-            let idCompany;
-            result.result[GOLOVNOY].map((i) => {
-              idCompany = i;
-            });
-            console.log(idCompany, item.ID);
-            fetch(
-              "https://itr24.bitrix24.ru/rest/1/54eoiyquodgp4a4g/crm.company.get.json",
-              {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ id: idCompany }),
-              }
-            )
-              .then((res) => res.json())
-              .then((result) => {
-                if (result.result[DOCHERNIY].length > 0) {
-                  console.log(result.result[DOCHERNIY]);
-                } else {
-                  fetch(
-                    "https://itr24.bitrix24.ru/rest/1/cghla29h9k1053w1/crm.company.update.json",
-                    {
-                      method: "POST",
-                      headers: {
-                        "Content-Type": "application/json",
-                      },
-                      body: JSON.stringify({
-                        id: idCompany,
-                        fields: {
-                          [DOCHERNIY]: item.ID,
-                        },
-                      }),
-                    }
-                  ).then((res) => res.json());
-                }
-              });
+const axios = require("axios");
+const webhook = `https://itr24.bitrix24.ru/rest/1/9fw61yqqc6se2xeq/`;
+let object = {};
+let arr = [];
+let newArr = [];
+CompanyGetList(0);
+async function addDocher(object) {
+  console.log(object);
+}
+
+async function CompanyGetList(lastId) {
+  // console.log(lastId, "lastId");
+  let { data } = await axios.get(webhook + "crm.company.list", {
+    params: {
+      order: { ID: "ASC" },
+      filter: { ">ID": lastId },
+      select: ["ID", GOLOVNOY],
+      start: -1,
+    },
+  });
+  if (data.result.length > 0) {
+    data.result.map((i) => {
+      if (i[GOLOVNOY]) {
+        object = { glav: i[GOLOVNOY], docher: i.ID };
+        arr.push(object);
+      }
+    });
+    lastId = data.result.at(-1)["ID"];
+    // await addDocher(object);
+    CompanyGetList(lastId);
+  } else {
+    arr.map((item) => {
+      if (newArr.length > 1) {
+        newArr.map((element) => {
+          // console.log(element.glav, item.glav)
+          if (element.glav == item.glav) {
+            // тут нужно удалить предыдущий элемент массива у которого такое же значение glav
+            console.log(newArr);
+            object = { glav: item.glav, docher: [item.docher, element.docher] };
+            console.log(object);
+            newArr.push(object);
           }
         });
+      } else {
+        newArr.push(item);
+      }
     });
-  });
+    console.log(arr,newArr, "finish");
+  }
+}
 
-//     if (result.result[GOLOVNOY]) {
-
-//
-//             console.log(result.result);
-
-//           });
-//       });
-//       // console.log(result.result[GOLOVNOY], item.ID);
-//     }
-//   });
