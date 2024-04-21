1ef4a638f327f8be100e956cfbf511fb3884f632
sergey <sapokrovsky@gmail.com>
2024-04-21 12:08:17 +0300

    init

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 const mainCompany = "UF_CRM_1713469133";
 const secondCompany = "UF_CRM_1713278591";
-
 const axios = require("axios");
 const webhook = `https://itr24.bitrix24.ru/rest/1/9fw61yqqc6se2xeq/`;
 let resaultCompany = [];
 
 CompanyGetList(0);
 
-
-async function addDocher(object) {
-  console.log(object);
+async function addSecondCompany(arr) {
+  console.log(arr);
+  arr.map((element) => {
+    console.log(element.mainCompany, element.secondCompany);
+    axios.get(webhook + "crm.company.update", {
+      params: {
+        id: element.mainCompany,
+        fields: {
+          [secondCompany]: element.secondCompany,
+        },
+      },
+    });
+  });
 }
 
 function parserArr(mCompany, sCompany) {
-  let indexMainCompany = resaultCompany.findIndex( (item) => {
+  let indexMainCompany = resaultCompany.findIndex((item) => {
     return item.mainCompany === mCompany;
   });
-  if(indexMainCompany == -1){
-    resaultCompany.push({'mainCompany': mCompany, 
-                         'secondCompany': [sCompany]});
+  if (indexMainCompany == -1) {
+    resaultCompany.push({ mainCompany: mCompany, secondCompany: [sCompany] });
   } else {
-   resaultCompany[indexMainCompany].secondCompany.push(sCompany);
+    resaultCompany[indexMainCompany].secondCompany.push(sCompany);
   }
 }
 
@@ -29,21 +37,20 @@ async function CompanyGetList(lastId) {
   let { data } = await axios.get(webhook + "crm.company.list", {
     params: {
       order: { ID: "ASC" },
-      filter: { ">ID": lastId, "!UF_CRM_1713469133" : '' },
+      filter: { ">ID": lastId, "!UF_CRM_1713469133": "" },
       select: ["ID", mainCompany],
       start: -1,
     },
   });
-  if (data.result.length > 0) {    
+  if (data.result.length > 0) {
     lastId = data.result.at(-1)["ID"];
-    for(key in data.result) {
-      parserArr(data.result[key].UF_CRM_1713469133, data.result[key].ID);    
+    for (key in data.result) {
+      parserArr(data.result[key].UF_CRM_1713469133, data.result[key].ID);
     }
     CompanyGetList(lastId);
     console.log(data.result);
   } else {
-    console.log(resaultCompany);
+    addSecondCompany(resaultCompany);
     console.log("finish");
   }
 }
-
